e5f8e243a4033a89408cbc08b1a9491bfe458b51
Jeremie <charlet.jeremie@gmail.com>
2015-11-27 22:03:32 +0000

    modif script to install latest Mongo DB 3

--- 201-dependency-between-scripts-using-extensions/mongo-configure-ubuntu.sh
+++ 201-dependency-between-scripts-using-extensions/mongo-configure-ubuntu.sh
@@ -1,3 +1,3 @@
 # Configure mongodb.list file with the correct location
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
--- 201-dependency-between-scripts-using-extensions/mongo-install-ubuntu.sh
+++ 201-dependency-between-scripts-using-extensions/mongo-install-ubuntu.sh
@@ -2,4 +2,5 @@
 sudo apt-get -y update
 
 #Install Mongo DB
-sudo apt-get install -y mongodb-10gen
+sudo apt-get install -y mongodb-org=3.0.7 mongodb-org-server=3.0.7 mongodb-org-shell=3.0.7 mongodb-org-mongos=3.0.7 mongodb-org-tools=3.0.7
+
